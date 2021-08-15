503d8c610b3779a6703e6d10dd2d7b141a2a42d0
hilmisarioglu <77903324+hilmisarioglu@users.noreply.github.com>
2021-08-15 17:39:33 +0200

    2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import HoverGlowEffect from './components/HoverGlowEffect'
+function App() {
+  return (
+    <div className="App">
+      <HoverGlowEffect/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HoverGlowEffect.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './HoverGlowEffect.css';
+function HoverGlowEffect() {
+    return (
+        <div>
+            <button class="glow-on-hover" type="button">Hover Glow Effect</button>
+            <br/>
+        </div>
+    )
+}
+export default HoverGlowEffect;
